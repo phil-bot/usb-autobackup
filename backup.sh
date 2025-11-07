4a1449383c5ed468651900d1868188c160f126fd
phil <github@der-ju.de>
2025-11-07 22:06:44 +0100

    add timestamps

--- backup.sh
+++ backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+function logger() {
+    printf '%s %s' "$(date)" "${@}"
+}
 
 # CONFIG
 SERVER=nas.lan
@@ -12,12 +15,9 @@ existingScriptLocation="$(realpath "$0")"
 tempScriptLocation=$(mktemp)
 
 echo -----------------------------------------------------
-date
-echo -----------------------------------------------------
-
 
 # Online Check
-printf 'Wait for github.com...'
+logger 'Wait for github.com...'
 until curl -s -f -o /dev/null "https://github.com"
 do
   sleep 5
@@ -30,32 +30,32 @@ wget -q -O "$tempScriptLocation" "$urlOfUpdatedVersion"
 
 # Update Check
 if cmp --silent -- "$existingScriptLocation" "$tempScriptLocation"; then
-    echo "No update needed."
+    logger "No script update needed."
 else
     # Replace the current script with the updated version
     if [[ -f "$tempScriptLocation" ]]; then
         mv "$tempScriptLocation" "$existingScriptLocation"
         chmod +x "$existingScriptLocation"
-        echo "Script updated successfully."
+        logger "Script updated successfully."
         UPDATED=true
         rm $tempScriptLocation
         exec $existingScriptLocation
     else
-        echo "Failed to download the updated script."
+        logger "Failed to download the updated script."
         rm $tempScriptLocation
         exit 1
     fi
 fi
 
 # Get Location
-printf 'Get location: '
+logger 'Get location: '
 ORT=$(curl -s https://ipinfo.io/city)
 POSTAL=$(curl -s https://ipinfo.io/postal)
 
 printf '%s (%s)\n' "${ORT}" "${POSTAL}"
 
 # Check for Server
-printf 'Wait for %s...' "${SERVER}"
+logger 'Wait for %s...' "${SERVER}"
 while ! ping -c 1 -n -w 1 ${SERVER} &> /dev/null
 do
     #waiting..
@@ -74,20 +74,20 @@ if [[ $UPDATED == true ]]; then
     /usr/bin/telegram-send -M "*${ICH} updated successfully* (Version: ${Version}). \U0001f680"
 fi
 
-echo "umount ${TARGET_DIR}"
+logger "umount ${TARGET_DIR}"
 /usr/bin/umount ${TARGET_DIR}
 
-echo "create ${TARGET_DIR}..."
+logger "create ${TARGET_DIR}..."
 /usr/bin/mkdir -p ${TARGET_DIR} || { /usr/bin/telegram-send -M "*${ICH} meldet einen FEHLER* \U00026A0 \n\nKann den Einhängepunkt nicht erstellen\."; exit; }
 
-echo "mount ${TARGET_DIR}"
+logger "mount ${TARGET_DIR}"
 /usr/bin/mount ${MOUNT_POINT} ${TARGET_DIR} || { /usr/bin/telegram-send -M "*${ICH} meldet einen FEHLER* \U00026A0 \n\nFestplatte nicht gefunden\.\."; exit; }
 
 sleep 3
 
 /usr/bin/telegram-send -M "*${ICH} meldet sich zum Dienst* \U0001f596 \n\nVerbindung mit *${SERVER//./\\.}* erfolgreich hergestellt\.\n\nBackup wird jetzt gestartet\."
 
-echo "start rsync..."
+logger "start rsync..."
 
 SECONDS=0
 
@@ -95,18 +95,20 @@ SECONDS=0
 
 duration=$SECONDS
 
-echo "rsync done..."
+logger "rsync done..."
 
 sleep 3
 
-echo "umount ${TARGET_DIR}"
+logger "umount ${TARGET_DIR}"
 /usr/bin/umount ${TARGET_DIR} || { /usr/bin/telegram-send -M "*${ICH} meldet einen FEHLER* \U00026A0 \n\nKonnte die Festplatte nicht aushängen\."; exit; }
 /usr/bin/telegram-send -M "*${ICH} hat das Backup erfolgreich beendet* \U0001F64C \n\nGedauert hat das ganze $((duration / 60)) Minuten\.\n\nIch lege mich wieder Schlafen\."
 sleep 5
 
 ##echo shutdown.. bye bye..
+logger "put HD to sleep"
 hdparm -y /dev/sda
 
+logger "disconnect from VPN"
 /usr/bin/nmcli con down id wg0
 
 #shutdown now
